cc931918e3605b5ec7fc62b49fbf102492800e4e
Lisa <lisa.das9374@gmail.com>
2022-12-23 12:33:01 +0530

    [Lisa] Add app files

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+
+
+app = FastAPI()
+    
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/get")
+async def get_name(name:str):
+    return {"name": name}
+
